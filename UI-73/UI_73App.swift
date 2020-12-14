51c568bf6830ccd62d37519f8376906b84b89bb1
nyannya0328 <thecuba54@gmail.com>
2020-12-14 23:32:51 +0900

    UI-73

---
+++ UI-73/UI_73App.swift
@@ -0,0 +1,17 @@
+//
+//  UI_73App.swift
+//  UI-73
+//
+//  Created by にゃんにゃん丸 on 2020/12/14.
+//
+
+import SwiftUI
+
+@main
+struct UI_73App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
